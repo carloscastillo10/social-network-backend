7a574ff02acad2af4e48c83d4a978e1c4d260adb
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-30 15:29:19 -0500

    :sparkles: Adding post crud

--- app/v1/post-microservice/api/v1/components/post/controller.js
+++ app/v1/post-microservice/api/v1/components/post/controller.js
@@ -1,3 +1,5 @@
+const error = require('../../../../../utils/error');
+const uuid = require('uuid');
 const table = 'post';
 
 class PostController {
@@ -11,6 +13,52 @@ class PostController {
 
     list() {
         return this.store.list(table);
+    }
+
+    async get(id) {
+        const user = await this.store.get(table, id);
+        if (!user) {
+            throw error('Post not found', 404);
+        }
+
+        return user;
+    }
+
+    async upsert(data, user) {
+        const post = {
+            id: data.id,
+            user: user,
+            text: data.text,
+        };
+
+        if (!post.id) {
+            post.id = uuid.v1();
+        }
+
+        return this.tore.upsert(table, post).then(() => post);
+    }
+
+    async like(post, user) {
+        const like = await this.tore.upsert(`${table}_like`, {
+            post: post,
+            user: user,
+        });
+
+        return like;
+    }
+
+    async postsLiked(user) {
+        const users = await this.tore.query(`${table}_like`, { user: user });
+        return users;
+    }
+
+    async postLikers(post) {
+        const users = await this.store.query(
+            `${table}_like`,
+            { post: post },
+            { post: post }
+        );
+        return users;
     }
 }
 
--- app/v1/post-microservice/api/v1/components/post/network.js
+++ app/v1/post-microservice/api/v1/components/post/network.js
@@ -1,10 +1,17 @@
+const authSecure = require('./secure');
 const controller = require('./index');
 const express = require('express');
 const response = require('../../../../../network/response');
 
 const router = express.Router();
 
-router.get('/', list);
+router.get('/', authSecure('list'), list);
+router.get('/like', authSecure('list_own'), postsLiked);
+router.get('/:id', authSecure('get'), get);
+router.post('/', authSecure('add',), upsert);
+router.put('/', authSecure('update', { owner: 'user' }), upsert);
+router.post('/:id/like', authSecure('add'), like);
+router.get('/:id/like', authSecure('list'), postLikers);
 
 function list(req, res, next) {
     controller
@@ -13,6 +20,47 @@ function list(req, res, next) {
             response.success(req, res, 200, posts);
         })
         .catch(next);
+}
+
+function get(req, res, next) {
+    controller
+        .get(req.params.id)
+        .then((post) => {
+            response.success(req, res, 200, post);
+        })
+        .catch(next);
+}
+
+function upsert(req, res, next) {
+	controller.upsert(req.body, req.user.id)
+		.then((post) => {
+			response.success(req, res, 201, post);
+		})
+		.catch(next);
+}
+
+function like(req, res, next) {
+	controller.like(req.params.id, req.user.sub)
+		.then((post) => {
+			response.success(req, res, 201, post);
+		})
+		.catch(next);
+}
+
+function postLikers(req, res, next) {
+	controller.postLikers(req.params.id)
+		.then((post) => {
+			response.success(req, res, 200, post);
+		})
+		.catch(next);
+}
+
+function postsLiked(req, res, next) {
+	controller.postsLiked(req.user.sub)
+		.then((post) => {
+			response.success(req, res, 200, post);
+		})
+		.catch(next);
 }
 
 module.exports = router;
