6aef7e055e97307595661a54459e0c94e8b5cc0f
carloscastillo10 <carloscastillo090916@gmail.com>
2022-09-30 10:55:28 -0500

    :sparkles: Adding mysql microservice

--- app/v1/config/config.js
+++ app/v1/config/config.js
@@ -10,6 +10,9 @@ const config = {
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
     },
+    mysqlMicroService: {
+        port: process.env.MYSQL_MICROSERVICE_PORT || 3001
+    },
     port: process.env.PORT || 3000,
 };
 
